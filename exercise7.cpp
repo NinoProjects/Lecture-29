fc65fda1e703fb1714fa3ddb319187021c68e96a
NinoProjects <121447717+NinoProjects@users.noreply.github.com>
2023-02-11 19:18:40 +0400

    Add files via upload

---
+++ exercise7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Whats your name? " <<endl;
+    string name;
+	cin >> name;
+	
+	cout << "Hello " << name <<endl;
+    
+    
+    
+    return 0;
+}
